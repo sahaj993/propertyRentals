4032676a5b7299f2101e4a8921537e6c6a197ca9
sahaj993 <sahajseth1998@gmail.com>
2020-11-19 11:26:27 +0530

    Folder structure set up

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const Router = express.Router();
 
+const homeController = require('../controller/main_controller')
 
+Router.get('/', homeController.home);
 
 module.exports = Router;
